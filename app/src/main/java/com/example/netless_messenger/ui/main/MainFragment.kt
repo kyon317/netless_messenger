6c913e701eb2ded8cfeb3683dd595a2a56f19ad7
Dushyant <Dchauhan@sfu.ca>
2022-11-14 20:16:06 -0800

    Main screen and float button implemented TODO: Dialog fragment and message activity/fragment

--- app/src/main/java/com/example/netless_messenger/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/netless_messenger/ui/main/MainFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.netless_messenger.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainFragment : Fragment() {
 
@@ -15,12 +19,25 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel : MainViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var floatButton: FloatingActionButton
+    private var exampleList: ArrayList<String> = arrayListOf("Jane Doe", "James Doe","Peter Parker")
 
     override fun onCreateView(
         inflater : LayoutInflater, container : ViewGroup?,
         savedInstanceState : Bundle?
     ) : View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val mainFragmentView =  inflater.inflate(R.layout.fragment_main, container, false)
+
+        floatButton = mainFragmentView.findViewById(R.id.floatingActionButton)
+        recyclerView = mainFragmentView.findViewById(R.id.mainFragRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = RecyclerViewAdapter(exampleList)
+
+        floatButton.setOnClickListener(){
+            Toast.makeText(activity, "Floating Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+        return mainFragmentView
     }
 
     override fun onActivityCreated(savedInstanceState : Bundle?) {
@@ -28,5 +45,6 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
 
 }
