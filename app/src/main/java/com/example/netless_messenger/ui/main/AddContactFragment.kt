3534fb82f3de6ce6d207af60990347e9f0431dfd
Reynolds398 <reynoldladestu@gmail.com>
2022-11-24 17:39:45 -0800

    Created add_contact_dialog.xml
    Removed unused imports and created an empty add_contact_dialog.xml

--- app/src/main/java/com/example/netless_messenger/ui/main/AddContactFragment.kt
+++ app/src/main/java/com/example/netless_messenger/ui/main/AddContactFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.netless_messenger.MainActivity
 import com.example.netless_messenger.R
 
