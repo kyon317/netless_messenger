7c2c5477584a51489f032e7c354759d5b92fb020
Reynolds398 <reynoldladestu@gmail.com>
2022-11-25 16:45:10 -0800

    Implemented back button in chat activity
    Back button in ChatActivity now returns to Main Activity

--- app/src/main/java/com/example/netless_messenger/ChatActivity.kt
+++ app/src/main/java/com/example/netless_messenger/ChatActivity.kt
@@ -1,14 +1,24 @@
 package com.example.netless_messenger
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class ChatActivity: AppCompatActivity() {
+    private lateinit var backArrow: ImageView
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.chat_activity_layout)
 
         //Hide default action bar
         supportActionBar?.hide()
+
+        //Return to Main Activity if backArrow is pressed
+        backArrow = findViewById(R.id.back_arrow_icon)
+        backArrow.setOnClickListener {
+            finish()
+        }
+
     }
 }
