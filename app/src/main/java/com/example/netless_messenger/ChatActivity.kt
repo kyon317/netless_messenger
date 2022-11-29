20c1283e207a48d6d27d9b400b2695722a671323
Dushyant <Dchauhan@sfu.ca>
2022-11-29 12:29:13 -0800

    Message UI Components -> Recycler View, Chat Box, etc. added

--- app/src/main/java/com/example/netless_messenger/ChatActivity.kt
+++ app/src/main/java/com/example/netless_messenger/ChatActivity.kt
@@ -1,24 +1,64 @@
 package com.example.netless_messenger
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.netless_messenger.ui.main.MessageViewAdapter
 
 class ChatActivity: AppCompatActivity() {
-    private lateinit var backArrow: ImageView
+
+    private lateinit var editText: EditText
+    private lateinit var sendButton: ImageView
+    private lateinit var messageRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.chat_activity_layout)
+        editText = findViewById(R.id.chat_input_edit_text)
+        sendButton = findViewById(R.id.send_button)
+        messageRecyclerView = findViewById(R.id.message_list)
+        messageRecyclerView.layoutManager = LinearLayoutManager(this)
+
+
+        val uName = intent.getStringExtra("userName").toString()
+
+        //Temp structure
+        val tempMessageList = ArrayList<String>()
+
+        supportActionBar?.title = uName
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
 
         //Hide default action bar
-        supportActionBar?.hide()
+        //supportActionBar?.hide()
 
         //Return to Main Activity if backArrow is pressed
-        backArrow = findViewById(R.id.back_arrow_icon)
-        backArrow.setOnClickListener {
-            finish()
+        //menuTitle.text = uName
+        //backArrow.setOnClickListener {
+            //finish()
+        //}
+
+        //TEMP FEATURE
+        sendButton.setOnClickListener(){
+            val message = editText.text.toString()
+            if(message != ""){
+                tempMessageList.add(message)
+                messageRecyclerView.adapter = MessageViewAdapter(tempMessageList)
+                editText.setText("")
+            }
         }
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
